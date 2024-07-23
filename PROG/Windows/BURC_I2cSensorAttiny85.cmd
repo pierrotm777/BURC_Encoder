24c1d636553a90911bb77c9ef4b03d4f8979ebf2
pierrotm777 <pierrotm777@gmail.com>
2024-07-23 14:56:00 +0200

    update firmwares

--- PROG/Windows/BURC_I2cSensorAttiny85.cmd
+++ PROG/Windows/BURC_I2cSensorAttiny85.cmd
@@ -1,2 +1,2 @@
-C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -pattiny85 -cusbasp -Pusb -Uflash:w:C:\tmp\_OpenAVRc\BURC_Projects\Firmwares_ProMini_Attiny85\HallAna2A1335_V2_1.hex:i 
+C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -pattiny85 -cusbasp -Pusb -Uflash:w:C:\tmp\_OpenAVRc\BURC_Projects\Firmwares_ProMini_Attiny85\HallAna2A1335_V2_2.hex:i 
 
