2a5b16cf393493ecdec095deaf5bd046896b52f3
pierrotm777 <pierrotm777@gmail.com>
2024-11-11 16:32:42 +0100

    Update BURC_I2cSensorAttiny85.cmd

--- PROG/Windows/BURC_I2cSensorAttiny85.cmd
+++ PROG/Windows/BURC_I2cSensorAttiny85.cmd
@@ -1,2 +1,3 @@
-C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -pattiny85 -cusbasp -Pusb -Uflash:w:C:\tmp\_OpenAVRc\BURC_Projects\Firmwares_ProMini_Attiny85\HallAna2A1335_V2_2.hex:i 
+C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -pattiny85 -cusbasp -Pusb -Uflash:w:"C:\Users\pierrot\Documents\GitHub\BURC_Encoder\PROG\Windows\HallAna2A1335_V2_2.hex":a -U lfuse:w:0XF1:m -U hfuse:w:0XDF:m -U efuse:w:0XFF:m 
+
 
