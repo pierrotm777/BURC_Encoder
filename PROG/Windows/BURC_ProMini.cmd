fe0304c78684746baab59c1d723e97a5c8351c4b
pierrotm777 <pierrotm777@gmail.com>
2025-04-13 17:24:31 +0200

    update firmware code

--- PROG/Windows/BURC_ProMini.cmd
+++ PROG/Windows/BURC_ProMini.cmd
@@ -1,2 +1,2 @@
-C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -patmega328p -carduino -PCOM5 -b57600 -D -Uflash:w:C:\tmp\_OpenAVRc\BURC_Projects\Firmwares_ProMini_Attiny85\BURC_CODER_V0_14.hex:i 
+C:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/bin/avrdude -CC:\Users\pierrot\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino17/etc/avrdude.conf -v -patmega328p -carduino -PCOM6 -b57600 -D -Uflash:w:C:\tmp\_OpenAVRc\BURC_Projects\Firmwares_ProMini_Attiny85\BURC_CODER_V0_14.hex:i 
 
